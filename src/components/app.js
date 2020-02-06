aa34bfe7e5f42a54ef6541822649b3d018bc7102
Dzmitry Malakhouski <dzmitry.malakhouski@tut.by>
2020-02-06 14:34:53 +0300

    feat: webpack

---
+++ src/components/app.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+    };
+  }
+  render() {
+    return (
+      <div>Calc</div>
+    );
+  }
+}
+
+export default App;
